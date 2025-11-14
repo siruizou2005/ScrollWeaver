7cc09350ff62bb60ecdbbd7cf2bbee6146849322
fQwQf <supertjz123@foxmail.com>
2025-11-14 14:01:55 +0800

    feat: 在历史消息中插入当前事件提示，增强场景事件的可视化效果

--- modules/llm/Gemini.py
+++ modules/llm/Gemini.py
@@ -16,8 +16,8 @@ class Gemini(BaseLLM):
     - gemini-2.0-flash
     - gemini-1.5-flash
     - gemini-1.5-pro
-    - gemini-2.5-flash-preview-04-17
-    - gemini-2.5-pro-preview-05-06
+    - gemini-2.5-flash
+    - gemini-2.5-pro
     """
     
     def __init__(self, model="gemini-2.0-flash", timeout: Optional[int] = 20, display_name: Optional[str] = None):
